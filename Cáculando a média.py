19f3627fcfbcd756007c2688a6bf2accc45dfb5e
Pedro Barbosa <156959341+Dilectus-a-Deo@users.noreply.github.com>
2024-04-23 13:43:00 -0300

    Add files via upload

---
+++ Cáculando a média.py
@@ -0,0 +1,13 @@
+import statistics
+
+#Dados 
+
+dados = [19.02,19.04,19.06,19.04,19.04]
+
+#Média 
+
+media = statistics.mean(dados)
+
+#imprimindo o valor 
+
+print(f" Média: {media} +/- 0.02 mm ")
