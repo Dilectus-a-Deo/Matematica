ba8a82f1427618bee96be973312e9738573874dc
Pedro Barbosa <156959341+Dilectus-a-Deo@users.noreply.github.com>
2024-04-23 13:34:30 -0300

    Add files via upload

---
+++ Estatística das alunas.py
@@ -0,0 +1,30 @@
+#Dados das alturas das mulhres
+
+import statistics
+
+#Dados em sala de aula
+
+dados = [47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 65, 67]
+
+#média
+
+media = statistics.mean(dados)
+
+#mediana
+
+mediana = statistics.mean(dados)
+
+#desvio medio
+
+desvio_medio = statistics.mean(dados)
+
+#desvio padrao
+
+desvio_padrao = statistics.mean(dados)
+
+#imprimindo resultados
+
+print(f" Desvio médio: {desvio_medio} ")
+print(f" Mediana: {mediana} ")
+print(f" Média: {media} ")
+print(f" Desvio Padrão : {desvio_padrao} ")
